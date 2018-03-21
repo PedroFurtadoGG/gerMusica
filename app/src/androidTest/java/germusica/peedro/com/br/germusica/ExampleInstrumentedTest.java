89daa8a569747f5a618490405cf195d307166cad
sedordiga <sedordiga@gmail.com>
2018-03-21 13:58:29 -0300

    Initial commit

---
+++ app/src/androidTest/java/germusica/peedro/com/br/germusica/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package germusica.peedro.com.br.germusica;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("germusica.peedro.com.br.germusica", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/germusica/peedro/com/br/germusica/Artist.java
@@ -0,0 +1,17 @@
+package germusica.peedro.com.br.germusica;
+
+public class Artist {
+
+    private String artistId;
+    private String artistName;
+    private String artistGenre;
+
+    public Artist(){}
+
+    public Artist(String artistId, String artistName, String artisGenre){
+        this.artistId = artistId;
+        this.artistName = artistName;
+        this.artistGenre = artistGenre;
+
+    }
+}
