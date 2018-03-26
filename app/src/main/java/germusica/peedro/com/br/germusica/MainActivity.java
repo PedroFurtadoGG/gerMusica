48da729f4216a6aa5082159ac35f4c6ca4b65afd
sedordiga <sedordiga@gmail.com>
2018-03-26 19:51:07 -0300

    list view contendo dados do banco

--- app/src/main/java/germusica/peedro/com/br/germusica/MainActivity.java
+++ app/src/main/java/germusica/peedro/com/br/germusica/MainActivity.java
@@ -6,11 +6,17 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseReference databaseArtists;
 
+    ListView listViewArtists;
+
+    List<Artist> artistList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
        databaseArtists = FirebaseDatabase.getInstance().getReference("Artists");
 
-       editTextName     =  (EditText) findViewById(R.id.editTextName);
-       buttonAddArtist  =  (Button) findViewById(R.id.buttonAddArtist);
-       spinnerGenres    =  (Spinner) findViewById(R.id.spinnerGenres);
+       editTextName     =  findViewById(R.id.editTextName);
+       buttonAddArtist  =  findViewById(R.id.buttonAddArtist);
+       spinnerGenres    =  findViewById(R.id.spinnerGenres);
 
        buttonAddArtist.setOnClickListener( new View.OnClickListener(){
 
@@ -41,15 +51,40 @@ public class MainActivity extends AppCompatActivity {
        });
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        databaseArtists.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                artistList.clear();
+
+                for(DataSnapshot artistSnapshot : dataSnapshot.getChildren()){
+
+                    Artist artist = artistSnapshot.getValue(Artist.class);
+
+                    artistList.add(artist);
+                }
+                ArtistListAdapter adapter = new ArtistListAdapter(MainActivity.this, artistList);
+
+                listViewArtists.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     private void addArtist(){
         String name = editTextName.getText().toString().trim();
         String genre = spinnerGenres.getSelectedItem().toString().trim();
 
         if(!TextUtils.isEmpty(name)){
             String id = databaseArtists.push().getKey();
-
             Artist artist = new Artist(id, name, genre);
-
             databaseArtists.child(id).setValue(artist);
 
             Toast.makeText(this, "Artista Adicionado", Toast.LENGTH_LONG).show();
