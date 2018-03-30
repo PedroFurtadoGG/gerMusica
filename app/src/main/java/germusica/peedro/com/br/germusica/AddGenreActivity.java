bed79bd417785cecfd276dbb2a0b1033397a925b
sedordiga <sedordiga@gmail.com>
2018-03-30 11:25:22 -0300

    criado xml estatico da view de add genre

---
+++ app/src/main/java/germusica/peedro/com/br/germusica/AddGenreActivity.java
@@ -0,0 +1,13 @@
+package germusica.peedro.com.br.germusica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddGenreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_genre);
+    }
+}
